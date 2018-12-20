f4d0983a2e3395dd4934676d61448664fc0851a8
Abhirup Subba <Abhirup Subba@Devi.attlocal.net>
2018-12-20 02:58:19 -0600

    Changin Class Name

--- MavenTest/src/test/java/MavenTestClass.java
+++ MavenTest/src/test/java/MavenTestClass.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 		import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 		
-public class Cllsa {
+public class MavenTestClass {
 
 	public static void main(String[] args) {
 	
