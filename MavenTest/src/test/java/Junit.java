44b15eb624314afb4e6f7ad9982b7860009b51a7
Abhirup Subba <Abhirup Subba@Devi.attlocal.net>
2018-12-20 12:56:29 -0600

    Adding Maven plugin in Pox.xml

---
+++ MavenTest/src/test/java/Junit.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit {
+
+	@Test
+	public void test() {
+		MavenTestClass obj = new MavenTestClass();
+		
+		obj.TestCase();
+	}
+
+}
