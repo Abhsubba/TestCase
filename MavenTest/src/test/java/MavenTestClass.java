984f35a9d08576ce93259009fe56c252ffbd4717
Abhirup Subba <Abhirup Subba@Devi.attlocal.net>
2018-12-20 03:08:14 -0600

    Addding TestCase

--- MavenTest/src/test/java/MavenTestClass.java
+++ MavenTest/src/test/java/MavenTestClass.java
@@ -10,7 +10,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class MavenTestClass {
 
 	public static void main(String[] args) {
+		TestCase();
+		/*System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhirup Subba\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		 // launch Fire fox and direct it to the Base URL
+        driver.get("https://www.target.com/");
+				
+			     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				driver.close();
+		    */
 	
+		
+	}
+	
+	public static void TestCase() {
+		
 		System.setProperty("webdriver.gecko.driver","C:\\Users\\Abhirup Subba\\Downloads\\geckodriver-v0.23.0-win64\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		
@@ -20,7 +35,6 @@ public class MavenTestClass {
 			     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				driver.close();
 		    
-		
 		
 	}
 
